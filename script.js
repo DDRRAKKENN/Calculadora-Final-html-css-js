7e90317a19028503855b7ac3fc69306bd8abfd3a
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-27 17:29:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function servicio() {
     document.getElementById("eleccion").innerHTML =
         "Usted eligio el servicio " +
         document.getElementById("sel").value +
-        ", Muchas gracias.";
+        ", nos vemos.";
 }
