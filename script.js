2d00b7401479ae40ab295487e61a141b371f89c7
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-27 16:56:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+function servicio() {
+    document.getElementById("eleccion").innerHTML =
+        "Usted eligio el servicio " +
+        document.getElementById("sel").value +
+        ", Muchas gracias.";
+}
