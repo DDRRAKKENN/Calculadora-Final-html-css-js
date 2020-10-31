f594536c399557db7fd7a9bddc881c77eea505ac
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-10-31 12:41:51 -0300

    correcion de profesor y nuevo js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-'use strict'
+"use strict";
 
-function servicio(){
-    document.getElementById("eleccion").innerHTML =
-        "Usted eligio el servicio " +
-        document.getElementById("sel").value +
-        ", Muchas gracias!.";
+function servicio() {
+  document.getElementById("eleccion").innerHTML =
+    "Usted eligio el servicio " +
+    document.getElementById("sel").value +
+    ", Muchas gracias.";
+}
+
+function seguro() {
+  document.getElementById("seleccionado").innerHTML = document.getElementById(
+    "sel"
+  ).value;
 }
